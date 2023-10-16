9eaa07b213470cc63a0ac83307cb501cff00e44a
fvolpisenac <fernando.volpi@alunos.sc.senac.br>
2023-10-16 19:47:05 -0300

    início MVC
    Criando estrutura MVC para projeto

---
+++ cantinscoffee/src/main/java/com/projetofinal/cantinscoffee/CantinscoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.projetofinal.cantinscoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantinscoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantinscoffeeApplication.class, args);
+	}
+
+}
