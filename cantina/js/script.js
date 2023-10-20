8f40f35976dd2367f788789e5c6d083adade3663
Murilo Schneider Santos <muriloschneider23@gmail.com>
2023-10-19 21:35:28 -0300

    Carrossel finalizado.

--- cantina/js/script.js
+++ cantina/js/script.js
@@ -1,30 +1,119 @@
-var swiper = new Swiper('.slide-content', {
+var swiperRecomendados = new Swiper('.slider-recomendados', {
     slidesPerView: 4,
+    spaceBetween: 25,
+    slidesPerGroup: 1,
     loop: false,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
+    centerSlide: true,
+    fade: true,
+    grabCursor: true,
     direction: getDirection(),
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next-recomendados',
+      prevEl: '.swiper-button-prev-recomendados',
     },
     on: {
       resize: function () {
-        swiper.changeDirection(getDirection());
+        swiperRecomendados.changeDirection(getDirection());
       },
     },
     breakpoints: {
         0: {
-            slidesPerView: 1,
-        },
-        760: {
             slidesPerView: 2,
         },
-        1000: {
+        1400: {
             slidesPerView: 3,
         },
-        1200: {
+        1700: {
+            slidesPerView: 4,
+        },
+    },
+});
+var swiperBebidas = new Swiper('.slider-bebidas', {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    slidesPerGroup: 1,
+    loop: false,
+    centerSlide: true,
+    fade: true,
+    grabCursor: true,
+    direction: getDirection(),
+    navigation: {
+      nextEl: '.swiper-button-next-bebidas',
+      prevEl: '.swiper-button-prev-bebidas',
+    },
+    on: {
+      resize: function () {
+        swiperBebidas.changeDirection(getDirection());
+      },
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        1400: {
+            slidesPerView: 3,
+        },
+        1700: {
+            slidesPerView: 4,
+        },
+    },
+});
+var swiperLanches = new Swiper('.slider-lanches', {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    slidesPerGroup: 1,
+    loop: false,
+    centerSlide: true,
+    fade: true,
+    grabCursor: true,
+    direction: getDirection(),
+    navigation: {
+      nextEl: '.swiper-button-next-lanches',
+      prevEl: '.swiper-button-prev-lanches',
+    },
+    on: {
+      resize: function () {
+        swiperLanches.changeDirection(getDirection());
+      },
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        1400: {
+            slidesPerView: 3,
+        },
+        1700: {
+            slidesPerView: 4,
+        },
+    },
+});
+var swiperDoces = new Swiper('.slider-doces', {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    slidesPerGroup: 1,
+    loop: false,
+    centerSlide: true,
+    fade: true,
+    grabCursor: true,
+    direction: getDirection(),
+    navigation: {
+      nextEl: '.swiper-button-next-doces',
+      prevEl: '.swiper-button-prev-doces',
+    },
+    on: {
+      resize: function () {
+        swiperDoces.changeDirection(getDirection());
+      },
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        1400: {
+            slidesPerView: 3,
+        },
+        1700: {
             slidesPerView: 4,
         },
     },
