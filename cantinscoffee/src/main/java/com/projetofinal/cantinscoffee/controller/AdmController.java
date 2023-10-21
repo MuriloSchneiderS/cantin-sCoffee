9a98d892b68f131f5120a11ff2a1a7d595b7a3f3
fvolpisenac <fernando.volpi@alunos.sc.senac.br>
2023-10-21 20:10:34 -0300

    Update AdmController.java

--- cantinscoffee/src/main/java/com/projetofinal/cantinscoffee/controller/AdmController.java
+++ cantinscoffee/src/main/java/com/projetofinal/cantinscoffee/controller/AdmController.java
@@ -3,6 +3,7 @@ package com.projetofinal.cantinscoffee.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
