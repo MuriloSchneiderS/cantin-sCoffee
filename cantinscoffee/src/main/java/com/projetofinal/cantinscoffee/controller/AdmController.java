0fadd1a85178d8f043807d48b36da61db67d99b6
fvolpisenac <fernando.volpi@alunos.sc.senac.br>
2023-10-21 20:10:27 -0300

    classe Adm

---
+++ cantinscoffee/src/main/java/com/projetofinal/cantinscoffee/controller/AdmController.java
@@ -0,0 +1,35 @@
+package com.projetofinal.cantinscoffee.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.projetofinal.cantinscoffee.model.Adm;
+import com.projetofinal.cantinscoffee.repository.AdmRepository;
+
+@Controller
+public class AdmController {
+    @Autowired
+    AdmRepository repository;
+
+    @GetMapping("/adm/login")
+    public String loginAdm() {
+        return "loginAdm";
+    }
+
+    @PostMapping("/loginAdm")
+    public String loginCliente(Adm adm) {
+        List<Adm> adms = new ArrayList<>();
+        adms = repository.findAll();
+
+        for (Adm a : adms) {
+            if (a.getLoginAdm().equals(adm.getLoginAdm()) && a.getSenhaAdm().equals(adm.getSenhaAdm())) {
+                return "redirect:/adm/cliente/lista";
+            }
+        }
+        return "redirect:/adm/login";
+    }
+}
